c46d53ffc1828efad3912e5dc4f496046eb2045f
KwakMirae <alfo9490@naver.com>
2023-08-17 17:43:48 +0900

    fix: import 추가

--- src/main/java/com/dnd/health/domain/profile/domain/Profile.java
+++ src/main/java/com/dnd/health/domain/profile/domain/Profile.java
@@ -7,16 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import lombok.*;
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+
+import javax.persistence.*;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
