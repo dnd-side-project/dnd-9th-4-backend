7e84cb292d3d406e467061b71b3f9b817186f879
KwakMirae <alfo9490@naver.com>
2023-08-19 15:52:50 +0900

    fix: ExerciseStyle 엔티티 id 컬럼명 수정

--- src/main/java/com/dnd/health/domain/profile/domain/ExerciseStyle.java
+++ src/main/java/com/dnd/health/domain/profile/domain/ExerciseStyle.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class ExerciseStyle {
 
     @Id
-    @Column(name = "exercise_style")
+    @Column(name = "exercise_style_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
